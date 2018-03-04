e7e38e76374206348e5ac54252cab6c6f66743da
PezeM <pezemos@gmail.com>
2018-03-04 17:20:12 +0100

    Basic user interface layer

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Andrzej", LastName = "Nowak" };
+            yield return new Friend { FirstName = "Przemek", LastName = "Stary" };
+            yield return new Friend { FirstName = "Blazej", LastName = "Hutek" };
+            yield return new Friend { FirstName = "Hubert", LastName = "Plis" };
+        }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private IFriendDataService _friendDataService;
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        private Friend _selectedFriend;
+
+        public Friend SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                _selectedFriend = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel(IFriendDataService friendDataSerice)
+        {
+            Friends = new ObservableCollection<Friend>();
+            _friendDataService = friendDataSerice;
+        }
+
+        public void Load()
+        {
+            var friends = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach (var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+    }
+}
