263e861ad0cac04b5abf964a6a3a06c91c6f7512
PezeM <pezemos@gmail.com>
2018-04-15 19:13:06 +0200

    Created meeting wrapper

---
+++ FriendOrganizer.UI/Wrapper/MeetingWrapper.cs
@@ -0,0 +1,37 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class MeetingWrapper : ModelWrapper<Meeting>
+    {
+        public int Id { get => Model.Id; }
+
+        public string Title
+        {
+            get => GetValue<string>();
+            set { SetValue(value); }
+        }
+
+        public DateTime DateFrom
+        {
+            get => GetValue<DateTime>();
+            set { SetValue(value); }
+        }
+
+        public DateTime DateTo
+        {
+            get => GetValue<DateTime>();
+            set { SetValue(value); }
+        }
+
+        public MeetingWrapper(Meeting model) : base(model)
+        {
+
+        }
+    }
+}
